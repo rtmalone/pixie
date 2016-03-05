2da5b85013329cc21affcaad469586b58bd02162
Tyler Malone <rtylermalone@gmail.com>
2016-03-04 23:40:40 -0600

    calls to pwinty

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -37,13 +37,56 @@ router.post('/send', function(req, res, next) {
 });
 
 router.post('/message', function(req, res, next) {
+  console.log('Twilio request to POST /message', req.body);
   var twiml = new twilio.TwimlResponse();
 
   twiml.message(function() {
-    this.body('Pixie');
-    this.media('http://i.imgur.com/Act0Q.gif');
+    this.body('Thanks!');
+    // this.media('http://i.imgur.com/Act0Q.gif');
   });
 
   res.type('text/xml');
   res.send(twiml.toString());
+
+  pwintyOrder(req.body);
 });
+
+var pwintyOrder = function(twilioImageData) {
+  var orderParams = {
+    recipientName: 'Tyler Malone',
+    address1: '1438 McAlpine Ave',
+    address2: '',
+    addressTownOrCity: 'Nashville',
+    stateOrCountry: 'TN',
+    postalOrZipCode: '37216',
+    countryCode: 'US',
+    desitnationCountryCode: 'US',
+    useTrackedShipping: false,
+    payment: 'InvoiceMe',
+    qualityLevel: 'Standard'
+  };
+
+  pwinty.createOrder(orderParams, function(err, order){
+    var photo = {
+      type: '4x4',
+      url: twilioImageData.MediaUrl0,
+      copies: '1',
+      sizing: 'crop',
+      priceToUser: '100'
+    };
+
+    if (err) {
+      res.render('error', {error: err, message: createOrder});
+    } else {
+      console.log('Pwinty order created: ', order);
+    }
+
+    pwinty.addPhotoToOrder(order.id, photo, function (err, order) {
+      if (err) {
+        res.render('error', {error: err, message: 'addPhotoToOrder'});
+      } else {
+        console.log('photo added: ', order);
+      }
+    });
+  });
+};
